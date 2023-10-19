8c290804182c6c7c962f6f2fc93efd31e5c39296
Geneovo <bodjuvwederhie@gmail.com>
2023-10-19 01:46:06 -0700

    printf main files

---
+++ README.md
@@ -0,0 +1,2 @@
+This is a printf group project
+
---
+++ main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdarg.h>
+#include <unistd.h>
+#include <limits.h>
+#include <stdlib.h>
+
+int _putchar(char c);
+
+
+#endif
